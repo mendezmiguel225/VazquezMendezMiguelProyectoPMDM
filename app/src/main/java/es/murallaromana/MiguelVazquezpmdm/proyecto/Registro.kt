d64e20353f77d9a68e9e27187403fb812e38cf94
mendezmiguel225 <mendezmiguel0238@gmail.com>
2021-10-15 13:20:34 +0200

    Creado el proyecto

---
+++ app/src/main/java/es/murallaromana/MiguelVazquezpmdm/proyecto/Registro.kt
@@ -0,0 +1,11 @@
+package es.murallaromana.MiguelVazquezpmdm.proyecto
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Registro : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_registro)
+    }
+}
