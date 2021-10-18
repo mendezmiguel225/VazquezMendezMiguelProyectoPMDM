82916239083d7247b6e5a05ad7a71aca5408e2a2
mendezmiguel225 <mendezmiguel0238@gmail.com>
2021-10-18 10:26:19 +0200

    Pantalla de registro

--- app/src/main/java/es/murallaromana/MiguelVazquezpmdm/proyecto/MainActivity.kt
+++ app/src/main/java/es/murallaromana/MiguelVazquezpmdm/proyecto/MainActivity.kt
@@ -1,11 +1,20 @@
 package es.murallaromana.MiguelVazquezpmdm.proyecto
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var bttSign:Button
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        bttSign =findViewById(R.id.bttSign)
+        bttSign.setOnClickListener {
+            val intent = Intent(this, Registro::class.java)
+            startActivity(intent)
+
+        }
     }
 }
