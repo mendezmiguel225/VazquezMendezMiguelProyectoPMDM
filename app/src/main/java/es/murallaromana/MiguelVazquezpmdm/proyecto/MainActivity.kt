2dc844c9803bee9a8a3a2047f159a7dd739735cc
mendezmiguel225 <mendezmiguel0238@gmail.com>
2021-10-19 10:25:25 +0200

    Shared

--- app/src/main/java/es/murallaromana/MiguelVazquezpmdm/proyecto/MainActivity.kt
+++ app/src/main/java/es/murallaromana/MiguelVazquezpmdm/proyecto/MainActivity.kt
@@ -4,9 +4,12 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import com.google.android.material.textfield.TextInputEditText
 
 class MainActivity : AppCompatActivity() {
     private lateinit var bttSign:Button
+    private lateinit var tiEmail:TextInputEditText
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -14,7 +17,7 @@ class MainActivity : AppCompatActivity() {
         bttSign.setOnClickListener {
             val intent = Intent(this, Registro::class.java)
             startActivity(intent)
-
         }
+
     }
 }
--- app/src/main/java/es/murallaromana/MiguelVazquezpmdm/proyecto/Registro.kt
+++ app/src/main/java/es/murallaromana/MiguelVazquezpmdm/proyecto/Registro.kt
@@ -1,11 +1,31 @@
 package es.murallaromana.MiguelVazquezpmdm.proyecto
 
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.ContactsContract
+import android.widget.Button
+import com.google.android.material.textfield.TextInputEditText
 
 class Registro : AppCompatActivity() {
+    //var sharedPref = getPreferences(MODE_PRIVATE)
+    //var editor = sharedPref.edit()
+    private lateinit var tiCorreo: TextInputEditText
+    private lateinit var bttConfirmar: Button
+    //editor.putString("email", getEmail.text.toString())
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_registro)
+        bttConfirmar = findViewById(R.id.bttConfirmar)
+        tiCorreo = findViewById(R.id.tiCorreo)
+        bttConfirmar.setOnClickListener {
+            var sharedPref = getPreferences(MODE_PRIVATE)
+            var editor = sharedPref.edit()
+            editor.putString("email",tiCorreo.toString())
+            onBackPressed()
+        }
     }
+
+
 }
