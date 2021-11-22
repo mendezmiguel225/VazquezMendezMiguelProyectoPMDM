1294ace962c03cfa59a008a40af848055c88b946
mendezmiguel225 <mendezmiguel0238@gmail.com>
2021-11-22 10:25:28 +0100

    SharedPreferences terminado

--- app/src/main/java/es/murallaromana/MiguelVazquezpmdm/proyecto/activities/MainActivity.kt
+++ app/src/main/java/es/murallaromana/MiguelVazquezpmdm/proyecto/activities/MainActivity.kt
@@ -1,34 +1,44 @@
 package es.murallaromana.MiguelVazquezpmdm.proyecto.activities
 
+import android.content.Context
 import android.content.Intent
+import android.content.SharedPreferences
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.Toast
 import com.google.android.material.textfield.TextInputEditText
 import es.murallaromana.MiguelVazquezpmdm.proyecto.R
 import es.murallaromana.MiguelVazquezpmdm.proyecto.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
-    private lateinit var bttSign:Button
-    private lateinit var bttSiguiente:Button
-    private lateinit var tiEmail:TextInputEditText
+
     private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         setTitle("Login")
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        binding= ActivityMainBinding.inflate(layoutInflater)
+        binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-        bttSign =binding.bttSign
-        bttSiguiente=binding.bttSiguiente
-        bttSign.setOnClickListener {
+
+        binding.bttRegistro.setOnClickListener {
             val intent = Intent(this, RegistroActivity::class.java)
             startActivity(intent)
         }
-        bttSiguiente.setOnClickListener {
-            val intent= Intent(this,ListaPeliculasActivity::class.java )
-            startActivity(intent)
+        binding.bttSiguiente.setOnClickListener {
+            val sharedPref: SharedPreferences = getSharedPreferences("datos", MODE_PRIVATE)
+            val contra = sharedPref.getString("contraseña","datos")
+            val email = sharedPref.getString("email","datos")
+            if (email != binding.tiEmail.text.toString()) {
+                Toast.makeText(this, "Email incorrecto", Toast.LENGTH_SHORT).show()
+            } else if (contra != binding.tiContrasenha.text.toString()) {
+                Toast.makeText(this, "Contraseña incorrecta", Toast.LENGTH_SHORT).show()
+            } else {
+                val intent = Intent(this, ListaPeliculasActivity::class.java)
+                startActivity(intent)
+            }
+
         }
 
 
--- app/src/main/java/es/murallaromana/MiguelVazquezpmdm/proyecto/activities/RegistroActivity.kt
+++ app/src/main/java/es/murallaromana/MiguelVazquezpmdm/proyecto/activities/RegistroActivity.kt
@@ -3,19 +3,21 @@ package es.murallaromana.MiguelVazquezpmdm.proyecto.activities
 import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Patterns
 import android.widget.Button
+import android.widget.Toast
 import androidx.core.graphics.toColor
 import com.google.android.material.textfield.TextInputEditText
 import es.murallaromana.MiguelVazquezpmdm.proyecto.R
 import es.murallaromana.MiguelVazquezpmdm.proyecto.databinding.ActivityPantallaDetalleBinding
 import es.murallaromana.MiguelVazquezpmdm.proyecto.databinding.ActivityRegistroBinding
 
+
 class RegistroActivity : AppCompatActivity() {
     //var sharedPref = getPreferences(MODE_PRIVATE)
     //var editor = sharedPref.edit()
-    private  lateinit var binding:ActivityRegistroBinding
-    private lateinit var tiCorreo: TextInputEditText
-    private lateinit var bttConfirmar: Button
+    private lateinit var binding: ActivityRegistroBinding
+
     //editor.putString("email", getEmail.text.toString())
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -23,13 +25,27 @@ class RegistroActivity : AppCompatActivity() {
         setTitle(R.string.registro)
         binding = ActivityRegistroBinding.inflate(layoutInflater)
         setContentView(binding.root)
-       binding.bttConfirmar.setOnClickListener {
-            var sharedPref = this.getSharedPreferences(R.string.file_preferences.toString(),Context.MODE_PRIVATE)
-            var editor = sharedPref.edit()
-            editor.putString("email",binding.tiCorreo.text.toString()).commit()
+        val pattern = Patterns.EMAIL_ADDRESS
+        binding.bttConfirmar.setOnClickListener {
+            if (binding.tiTelefono.text.toString() == "" || binding.tiContrasenha.text.toString() == "" ||
+                binding.tiCorreo.text.toString() == "" || binding.tiNombre.text.toString() == ""
+                || binding.tiNombreUs.toString() == ""
+            ) {
+                Toast.makeText(this, "Completa todos los campos", Toast.LENGTH_SHORT).show()
+            } else if (pattern.matcher(binding.tiCorreo.text.toString()).matches() == false) {
+                Toast.makeText(this, "Email no válido", Toast.LENGTH_SHORT).show()
+            } else if (binding.tiContrasenha.text.toString().length > 8 ||
+                binding.tiContrasenha.text.toString().length < 5
+            ) {
+                Toast.makeText(this, "La contraseña debe tener entre 5 y 8 caracteres", Toast.LENGTH_SHORT).show()
+            }else{
+                var sharedPref = getSharedPreferences("datos", Context.MODE_PRIVATE)
+                var editor = sharedPref.edit()
+                editor.putString("email", binding.tiCorreo.text.toString())
+                editor.putString("contraseña", binding.tiContrasenha.text.toString()).commit()
+                onBackPressed()
+            }
 
-            editor.putString("contraseña",binding.tiContrasenha.text.toString()).commit()
-            onBackPressed()
         }
     }
 }
